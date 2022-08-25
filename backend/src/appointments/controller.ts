ae100d9fa5867f67dac0979bd4ba7e98d0305a5e
carlosdartora <carlosdalsoler@gmail.com>
2022-08-25 01:14:41 -0300

    feat(routes/appointment): adiciona rotas create, update e delete

--- backend/src/appointments/controller.ts
+++ backend/src/appointments/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { Appointment } from "../types/user";
 import service from "./service";
 
 const getAll = async (_req: Request, res: Response) => {
@@ -8,4 +9,23 @@ const getAll = async (_req: Request, res: Response) => {
   res.status(StatusCodes.OK).send(appointments);
 }
 
-export default { getAll }
+const create = async (req: Request, res: Response) => {
+  const { body } = req;
+  await service.create(body as Appointment);
+  res.status(StatusCodes.CREATED).send({ message: "created successfully" });
+}
+
+const update = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { body } = req;
+  await service.update(body, id);
+  res.status(StatusCodes.OK).send({ message: "updated successfully" });
+}
+
+const destroy = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await service.destroy(id);
+  res.status(StatusCodes.OK).send({ message: "deleted successfully" })
+}
+
+export default { getAll, create, update, destroy }
