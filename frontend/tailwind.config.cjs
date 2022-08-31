f53a63689bd72f8b3d9caa4529a6018700a89934
carlosdartora <carlosdalsoler@gmail.com>
2022-08-31 15:59:26 -0300

    build(front/setup): configs do tailwind e postcss

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#f5f5f5',
+        primary: '#A5C9F4',
+        secundary: '#6083F2',
+        text: '#20323B',
+      },
+    },
   },
   plugins: [],
 }
