7f6e20b964d024ecaffda447004e6d10ad858f24
carlosdartora <carlosdalsoler@gmail.com>
2022-08-31 16:00:33 -0300

    feat(front/App.ts): cria componente de rotas com o react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
-function App() {
+import React from "react"
+import Routes from "./Routes"
+
+export default function App() {
   return (
-    <div />
+    <Routes />
   )
 }
 
-export default App
---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,17 @@
+import {
+  Routes as Switch, Route, Navigate
+} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Switch>
+  )
+};
