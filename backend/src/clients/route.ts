f6a32bc7801ced9f09f5e16737a25aceca56b697
carlosdartora <carlosdalsoler@gmail.com>
2022-09-11 19:34:14 -0300

    fix(client/routes): adiciona authMiddleware nas rotas

--- backend/src/clients/route.ts
+++ backend/src/clients/route.ts
@@ -8,7 +8,7 @@ const client = Router()
 
 client.get("/", authMiddleware, controller.getAll)
 client.post("/", authMiddleware, validateJoi(schemas.createClient), controller.create)
-client.put("/:id", validateJoi(schemas.updateClient), controller.update)
-client.delete("/:id", controller.destroy)
+client.put("/:id", authMiddleware, validateJoi(schemas.createClient), controller.update)
+client.delete("/:id", authMiddleware, controller.destroy)
 
 export default client;
